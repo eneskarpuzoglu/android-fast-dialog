c76c7ee95631e503934171c7a3021be33eba9d32
Muhammet Enes Karpuzoğlu <eneskarpuzoglu@gmail.com>
2021-02-02 15:02:56 +0300

    bug fixed

--- app/src/main/java/ka/enes/com/sample/MainActivity.java
+++ app/src/main/java/ka/enes/com/sample/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     public void progress(View view){
         FastDialog.p(this).progressText("Please Wait!").create().show();
